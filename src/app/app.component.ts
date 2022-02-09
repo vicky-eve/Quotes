2b0e46d28ad8869d339e71e1457b0f4f40c5d49d
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-09 15:45:47 +0300

    created a constructor function and given the empty quote array 3 string values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quote';
+  quote :string [];
+  constructor(){
+    this.quote = ['quote', 'author', 'name']
+  }
 }
